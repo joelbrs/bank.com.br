fd0523e8f4f4a3501497391877b2fd056e322aad
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 10:31:18 -0300

    feat[server]: ensure send confirmation email when register an user

--- apps/server/src/modules/user/mutations/register-user.ts
+++ apps/server/src/modules/user/mutations/register-user.ts
@@ -3,6 +3,9 @@ import { User, UserModel } from "../user-model";
 import { GraphQLNonNull, GraphQLString } from "graphql";
 import { UserType } from "../user-type";
 import { successField } from "@entria/graphql-mongo-helpers";
+import { randomUUID } from "crypto";
+import { env } from "@/config";
+import { resend, UserConfirmationTemplate } from "@/mail";
 
 type RegisterUserInput = Pick<
   User,
@@ -46,9 +49,26 @@ export const RegisterUserMutation = mutationWithClientMutationId({
       taxId,
     }).save();
 
+    const confirmationCode = randomUUID();
+
+    const confirmationLink = new URL("/confirmacao", env.API_BASE_URL);
+    confirmationLink.searchParams.set("code", confirmationCode);
+    confirmationLink.searchParams.set("redirect", env.AUTH_REDIRECT_URL);
+
+    await resend.emails.send({
+      from: "Bank <noreply@bank-woovi.joelf.tech>",
+      to: email,
+      subject: "[Bank] Link de Confirmação",
+      react: UserConfirmationTemplate({
+        email,
+        link: confirmationLink.toString(),
+      }),
+    });
+
     return {
       user,
-      success: "Operação realizada com sucesso!",
+      success:
+        "Operação realizada com sucesso! Verifique seu e-mail para confirmar a criação da sua conta.",
     };
   },
   outputFields: {
