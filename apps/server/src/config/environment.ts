441729d7a58af0c873c389df287cdff2942f88ed
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 09:11:03 -0300

    chore[server]: ensure environment variables are correctly defined

---
+++ apps/server/src/config/environment.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { z } from "zod";
+
+const environment_schema = z.object({
+  PORT: z.coerce.number().default(4000),
+  MONGO_DB_NAME: z.string(),
+  MONGO_URI: z.string(),
+  HASH_SALT: z.coerce.number().default(12),
+  JWT_KEY: z.string(),
+  RESEND_API_KEY: z.string(),
+  API_BASE_URL: z.string(),
+  AUTH_REDIRECT_URL: z.string(),
+});
+
+const _env = environment_schema.safeParse(process.env);
+
+if (!_env.success) {
+  console.log("Invalid environments variables: ", _env.error?.format());
+  throw new Error("Invalid environments variables");
+}
+
+export const env = _env.data;
