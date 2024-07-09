45671c32340b84a55256bef74cd8a99d45059842
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 15:30:37 -0300

    feat[server]: add account mongoose model

---
+++ apps/server/src/modules/account/account-model.ts
@@ -0,0 +1,40 @@
+import { Maybe } from "@repo/types/index";
+import mongoose, { Decimal128, Document } from "mongoose";
+
+export type Account = {
+  accountNumber: string;
+  taxId: string;
+  balance: Decimal128;
+  createdAt: Date;
+  updatedAt: Date;
+} & Document;
+
+type AccountDocument = Maybe<Document> & Account;
+
+const AccountSchema = new mongoose.Schema<Account>(
+  {
+    accountNumber: {
+      type: String,
+      unique: true,
+      default: Date.now().toString(),
+    },
+    taxId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    balance: {
+      type: mongoose.Schema.Types.Decimal128,
+      default: new mongoose.Types.Decimal128("0.0"),
+    },
+  },
+  {
+    collection: "Account",
+    timestamps: true,
+  }
+);
+
+export const AccountModel = mongoose.model<AccountDocument>(
+  "Account",
+  AccountSchema
+);
