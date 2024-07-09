959afaa49e44cf9348d05b707564529b3a8ab407
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 11:30:46 -0300

    feat[server]: add test environments

--- apps/server/jest.config.ts
+++ apps/server/jest.config.ts
@@ -7,7 +7,12 @@ import type { Config } from "jest";
 
 const config: Config = {
   clearMocks: true,
+  preset: "ts-jest",
   coverageProvider: "v8",
+  resetModules: false,
+  setupFiles: ["./test/jest-setup.ts"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  testEnvironment: "./test/environment/mongodb.ts",
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
   roots: ["<rootDir>/src"],
