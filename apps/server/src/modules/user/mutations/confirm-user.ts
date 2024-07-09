d632e138f5224ec0db8325f1fe6ec26e9f68b764
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 16:39:23 -0300

    feat[server]: ensure user is confirmed when access confirmation link

---
+++ apps/server/src/modules/user/mutations/confirm-user.ts
@@ -0,0 +1,57 @@
+import { ConfirmationLinkModel } from "../../../modules/confirmation-link";
+import { GraphQLNonNull, GraphQLString } from "graphql";
+import { mutationWithClientMutationId } from "graphql-relay";
+import { UserModel } from "../user-model";
+import { AccountModel } from "../../../modules/account";
+import { successField } from "@entria/graphql-mongo-helpers";
+
+type ConfirmUserInput = {
+  code: string;
+  redirect: string;
+};
+
+export const ConfirmUserMutation = mutationWithClientMutationId({
+  name: "ConfirmUser",
+  inputFields: {
+    code: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    redirect: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  mutateAndGetPayload: async ({ code }: ConfirmUserInput) => {
+    const existingCode = await ConfirmationLinkModel.findOne({ code });
+
+    if (!existingCode) {
+      throw new Error("Acesso não autorizado.");
+    }
+
+    const user = await UserModel.findOne({ taxId: existingCode.userTaxId });
+
+    if (!user) {
+      throw new Error("Usuário não encontrado.");
+    }
+
+    user.confirmed = true;
+
+    await Promise.all([
+      user?.save(),
+      AccountModel.create({
+        userTaxId: user.taxId,
+      }),
+      ConfirmationLinkModel.deleteOne({ code }),
+    ]);
+
+    return {
+      userId: user._id,
+    };
+  },
+  outputFields: {
+    userId: {
+      type: new GraphQLNonNull(GraphQLString),
+      resolve: async (payload) => (await payload)?.userId,
+      ...successField,
+    },
+  },
+});
--- apps/server/src/modules/user/mutations/index.ts
+++ apps/server/src/modules/user/mutations/index.ts
@@ -1,5 +1,7 @@
 import { RegisterUserMutation as RegisterUser } from "./register-user";
+import { ConfirmUserMutation as ConfirmUser } from "./confirm-user";
 
 export const UserMutations = {
   RegisterUser,
+  ConfirmUser,
 };
