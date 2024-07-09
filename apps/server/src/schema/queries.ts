c0d9bb9b07b7e1e10f5372e22ed1c9e8379ec173
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 14:30:16 -0300

    feat[server]: add query graphql ts

--- apps/server/src/schema/queries.ts
+++ apps/server/src/schema/queries.ts
@@ -1,7 +1,14 @@
-import { GraphQLObjectType } from "graphql";
+import { GraphQLObjectType, GraphQLString } from "graphql";
+import { globalIdField } from "graphql-relay";
 
 export const Query = new GraphQLObjectType({
   name: "Query",
   description: "Root queries",
-  fields: () => ({}),
+  fields: () => ({
+    id: globalIdField("Query"),
+    version: {
+      type: GraphQLString,
+      resolve: () => "1.0.0",
+    },
+  }),
 });
