72cebd26ab292f402e8e6ea82b54c0adcdb5da55
Joel Filho <joelfilho02812@gmail.com>
2024-07-09 17:54:03 -0300

    feat[server]: add context-middleware

--- apps/server/src/setup.ts
+++ apps/server/src/setup.ts
@@ -2,6 +2,7 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "kcors";
 import Router from "koa-router";
+import { contextMiddleware } from "./middlewares";
 
 export const setup = () => {
   const app = new Koa();
@@ -9,6 +10,8 @@ export const setup = () => {
 
   app.use(bodyParser());
   app.use(cors({ credentials: true }));
+
+  app.use(contextMiddleware);
 
   app.use(router.routes()).use(router.allowedMethods());
 
