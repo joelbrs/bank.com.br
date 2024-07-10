03fbac3f08d41a4e56e858ce76c1d5cd4a4fb619
Joel Filho <joelfilho02812@gmail.com>
2024-07-10 15:08:05 -0300

    feat[web]: link sign up graphql mutation

--- apps/web/relay.config.js
+++ apps/web/relay.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   src: "./src",
-  artifactDirectory: "./src/__generated__",
+  artifactDirectory: "./__generated__",
   language: "typescript",
   schema: "./data/schema.graphql",
   excludes: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
