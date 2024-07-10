0fa5430ad0af3b73f1efe692f758abd71943e344
Joel Filho <joelfilho02812@gmail.com>
2024-07-10 14:33:03 -0300

    refactor[web]: layout

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,35 +1,26 @@
-import { createBrowserRouter } from "react-router-dom"
-import { EmailSignInPage } from "./pages/sign-in/email-sign-in"
-import { AuthPageLayout } from "./pages/_layouts/auth"
-import { PasswordSignInPage } from "./pages/sign-in/password-sign-in"
-import { SignUpPage } from "./pages/sign-up"
+import { createBrowserRouter } from "react-router-dom";
+import { EmailSignInPage } from "./pages/sign-in/email-sign-in";
+import { AuthPageLayout } from "./layouts/auth";
+import { PasswordSignInPage } from "./pages/sign-in/password-sign-in";
+import { SignUpPage } from "./pages/sign-up";
 
 export const router: any = createBrowserRouter([
-    {
-        path: '/',
-        element: (
-            <AuthPageLayout />
-        ),
-        children: [
-            {
-                path: '/',
-                element: (
-                    <SignUpPage />
-                )
-            },
-            {
-                path: '/sign-in',
-                element: (
-                    <PasswordSignInPage />
-                )
-            },
-            {
-                path: '/sign-in/email',
-                element: (
-                    <EmailSignInPage />
-                )
-            }
-        ]
-    }
-])
-
+  {
+    path: "/",
+    element: <AuthPageLayout />,
+    children: [
+      {
+        path: "/",
+        element: <SignUpPage />,
+      },
+      {
+        path: "/sign-in",
+        element: <PasswordSignInPage />,
+      },
+      {
+        path: "/sign-in/email",
+        element: <EmailSignInPage />,
+      },
+    ],
+  },
+]);
