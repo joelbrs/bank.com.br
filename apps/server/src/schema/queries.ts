ef1ca56f0509ff56c77c100d23d2c802b761456b
Joel Filho <joelfilho02812@gmail.com>
2024-07-10 10:09:31 -0300

    feat[server]: add get logged user graphql query

--- apps/server/src/schema/queries.ts
+++ apps/server/src/schema/queries.ts
@@ -1,14 +1,40 @@
+import { nodeField, nodesField } from "../modules/node";
+import { UserLoader, UserType } from "../modules/user";
 import { GraphQLObjectType, GraphQLString } from "graphql";
-import { globalIdField } from "graphql-relay";
+import { connectionArgs, globalIdField } from "graphql-relay";
+import { isValidObjectId } from "mongoose";
+import { UnauthorizedException } from "@/exceptions";
 
 export const Query = new GraphQLObjectType({
   name: "Query",
   description: "Root queries",
   fields: () => ({
     id: globalIdField("Query"),
+    node: nodeField,
+    nodes: nodesField,
     version: {
       type: GraphQLString,
       resolve: () => "1.0.0",
+    },
+    user: {
+      type: UserType,
+      args: {
+        ...connectionArgs,
+      },
+      resolve: async (_, __, ctx) => {
+        const userId = ctx.user?._id;
+
+        if (!isValidObjectId(userId)) {
+          throw new UnauthorizedException();
+        }
+
+        const user = await UserLoader.load(ctx, userId?.toString());
+
+        if (!user) {
+          throw new UnauthorizedException();
+        }
+        return user;
+      },
     },
   }),
 });
