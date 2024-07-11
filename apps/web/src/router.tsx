0cc40439e942ea3ecdb0d795c7c4a2ca82afa1c7
Joel Filho <joelfilho02812@gmail.com>
2024-07-10 21:54:18 -0300

    feat[web]: add app layout and dashboard as routes

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -4,6 +4,9 @@ import { AuthPageLayout } from "./layouts/auth";
 import { PasswordSignInPage } from "./pages/sign-in/password-sign-in";
 import { SignUpPage } from "./pages/sign-up";
 import { ConfirmationPage } from "./pages/confirmation";
+import { DashboardPage } from "./pages/dashboard";
+import { AppLayout } from "./layouts/app";
+import { AuthProvider } from "./context/auth-context";
 
 export const router: any = createBrowserRouter([
   {
@@ -23,6 +26,20 @@ export const router: any = createBrowserRouter([
         element: <EmailSignInPage />,
       },
     ],
+  },
+  {
+    path: '/',
+    element: (
+      <AuthProvider>
+        <AppLayout />
+      </AuthProvider>
+    ),
+    children: [
+      {
+        path: '/dashboard',
+        element: <DashboardPage />
+      }
+    ]
   },
   {
     path: "/confirmation",
