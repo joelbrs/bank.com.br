997b0daa23d5264199aff53aa2ed8bf352e032a3
Joel Filho <joelfilho02812@gmail.com>
2024-07-11 16:02:22 -0300

    fea[web]: link recent transactions graphql query

--- apps/web/src/context/auth-context.tsx
+++ apps/web/src/context/auth-context.tsx
@@ -1,75 +1,74 @@
-import { ReactNode, createContext, useContext, useState } from "react"
-import { fetchQuery, graphql } from "relay-runtime"
-import { authContextQuery$data } from '../../__generated__/authContextQuery.graphql'
-import { environment } from "../relay"
+import { ReactNode, createContext, useContext, useState } from "react";
+import { fetchQuery, graphql } from "relay-runtime";
+import { authContextQuery$data } from "../../__generated__/authContextQuery.graphql";
+import { environment } from "../relay";
 import { toast } from "sonner";
 
 interface AuthContextData {
-    user: authContextQuery$data | null
-    getUser: (args?: {onError: Function}) => Promise<void>
+  user: authContextQuery$data | null;
+  getUser: (args?: { onError: Function }) => Promise<void>;
 }
 
-const AuthContext = createContext<AuthContextData | undefined>(undefined)
+const AuthContext = createContext<AuthContextData | undefined>(undefined);
 
-export const AuthProvider = ({children}: { children: ReactNode }) => {
-    const [user, setUser] = useState<authContextQuery$data | null>(null)
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<authContextQuery$data | null>(null);
 
-    const getUser = async (args?: {onError: Function}) => {
-        const query = graphql`
-            query authContextQuery {
-                account {
-                    balance
-                    accountNumber
-                    owner {
-                        fullName
-                        taxId
-                    }
-                }
-            }
-        `
+  const getUser = async (args?: { onError: Function }) => {
+    const query = graphql`
+      query authContextQuery {
+        account {
+          balance
+          accountNumber
+          owner {
+            fullName
+            taxId
+          }
+        }
+      }
+    `;
 
-        fetchQuery(environment, query, {}).subscribe({
-            next(value) {
-                if (!(value as authContextQuery$data).account) {
-                    toast.warning("Atenção!", {
-                        description:
-                          "Acesso não autorizado.",
-                        duration: 1500,
-                      });
+    fetchQuery(environment, query, {}).subscribe({
+      next(value) {
+        if (!(value as authContextQuery$data).account) {
+          toast.warning("Atenção!", {
+            description: "Acesso não autorizado.",
+            duration: 1500,
+          });
 
-                      if (args?.onError) {
-                        args.onError()
-                      }
-                    return 
-                }
+          if (args?.onError) {
+            args.onError();
+          }
+          return;
+        }
 
-                setUser(value as authContextQuery$data)
-            },
-            error() {
-                toast.error("Oops! Algo deu errado.", {
-                    description:
-                      "Verifique sua conexão com a internet e tente novamente mais tarde.",
-                    duration: 1500,
-                });
-                if (args?.onError) {
-                    args.onError()
-                  }
-                return 
-            },
-        })
-    }
-    
-    return (
-        <AuthContext.Provider value={{getUser, user}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+        setUser(value as authContextQuery$data);
+      },
+      error() {
+        toast.error("Oops! Algo deu errado.", {
+          description:
+            "Verifique sua conexão com a internet e tente novamente mais tarde.",
+          duration: 1500,
+        });
+        if (args?.onError) {
+          args.onError();
+        }
+        return;
+      },
+    });
+  };
+
+  return (
+    <AuthContext.Provider value={{ getUser, user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export const useAuth = () => {
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
 };
--- apps/web/src/pages/dashboard/recent-transactions.tsx
+++ apps/web/src/pages/dashboard/recent-transactions.tsx
@@ -1,45 +1,103 @@
+import { useEffect, useState } from "react";
+import { fetchQuery, graphql } from "relay-runtime";
+import { environment } from "../../relay";
+import { recentTransactionsQuery$data } from "../../../__generated__/recentTransactionsQuery.graphql";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components";
+
+type Transaction = {
+  _id: string;
+  value: string;
+  receiver: {
+    owner: {
+      fullName: string;
+      taxId: string;
+    };
+  };
+};
+
 export function RecentTransactions(): JSX.Element {
-    return (
-      <div className="space-y-8">
-        <div className="flex items-center">
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">Olivia Martin</p>
-            <p className="text-sm text-muted-foreground">
-              olivia.martin@email.com
-            </p>
-          </div>
-          <div className="ml-auto font-medium">+$1,999.00</div>
+  const [totalTransactions, setTotalTransactions] = useState(0);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  const query = graphql`
+    query recentTransactionsQuery {
+      transactions {
+        count
+        edges {
+          node {
+            _id
+            value
+            receiver {
+              owner {
+                fullName
+                taxId
+              }
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const fetchTransactions = () => {
+    fetchQuery(environment, query, {}).subscribe({
+      next: (values) => {
+        const { transactions } = values as recentTransactionsQuery$data;
+        const edges = transactions?.edges;
+
+        if (edges) {
+          const transactionList = edges?.map((item) => item?.node);
+
+          setTransactions(transactionList as Transaction[]);
+          setTotalTransactions(transactions.count as number);
+        }
+      },
+    });
+  };
+
+  const formatDocument = (cpfCnpj: string) => {
+    return `${cpfCnpj.slice(0, 3)}.***.***-${cpfCnpj.slice(9, 11)}`;
+  };
+
+  return (
+    <Card className="w-full">
+      <CardHeader>
+        <CardTitle>Transações Recentes</CardTitle>
+        <CardDescription>
+          Você realizou {totalTransactions} transações no último mês.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-5">
+          {transactions?.map(({ receiver, value, _id }) => (
+            <div key={_id} className="flex items-center">
+              <div className="space-y-1">
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {receiver?.owner?.fullName}
+                  </p>
+                  <p className="text-xs text-muted-foreground">
+                    {formatDocument(receiver?.owner?.taxId)}
+                  </p>
+                </div>
+              </div>
+              <div className="ml-auto font-medium">
+                +${Number(value).toFixed(2)}
+              </div>
+            </div>
+          ))}
         </div>
-        <div className="flex items-center">
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">Jackson Lee</p>
-            <p className="text-sm text-muted-foreground">jackson.lee@email.com</p>
-          </div>
-          <div className="ml-auto font-medium">+$39.00</div>
-        </div>
-        <div className="flex items-center">
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">Isabella Nguyen</p>
-            <p className="text-sm text-muted-foreground">
-              isabella.nguyen@email.com
-            </p>
-          </div>
-          <div className="ml-auto font-medium">+$299.00</div>
-        </div>
-        <div className="flex items-center">
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">William Kim</p>
-            <p className="text-sm text-muted-foreground">will@email.com</p>
-          </div>
-          <div className="ml-auto font-medium">+$99.00</div>
-        </div>
-        <div className="flex items-center">
-          <div className="ml-4 space-y-1">
-            <p className="text-sm font-medium leading-none">Sofia Davis</p>
-            <p className="text-sm text-muted-foreground">sofia.davis@email.com</p>
-          </div>
-          <div className="ml-auto font-medium">+$39.00</div>
-        </div>
-      </div>
-    );
-  }
+      </CardContent>
+    </Card>
+  );
+}
