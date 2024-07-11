5a65626daa9b691e30a45d4a0f154febeb6ffa51
Joel Filho <joelfilho02812@gmail.com>
2024-07-11 09:53:28 -0300

    feat[server]: add transaction mongoose model

---
+++ apps/server/src/modules/transaction/transaction-model.ts
@@ -0,0 +1,44 @@
+import { Maybe } from "@repo/types/index";
+import mongoose, { Decimal128, Document } from "mongoose";
+
+export type Transaction = {
+  senderTaxId: string;
+  receiverTaxId: string;
+  value: Decimal128;
+  description?: string;
+  createdAt: Date;
+  updated: Date;
+} & Document;
+
+type TransactionDocument = Maybe<Document> & Transaction;
+
+const TransactionSchema = new mongoose.Schema<Transaction>(
+  {
+    senderTaxId: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+    receiverTaxId: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+    value: {
+      type: mongoose.Schema.Types.Decimal128,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    collection: "Transaction",
+    timestamps: true,
+  }
+);
+
+export const TransactionModel = mongoose.model<TransactionDocument>(
+  "Transaction",
+  TransactionSchema
+);
