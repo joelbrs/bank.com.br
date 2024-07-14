edc99e4439562c3597605cc7061b959f3e6f715d
Joel Filho <joelfilho02812@gmail.com>
2024-07-14 17:02:51 -0300

    fix[web]: auth layout responsivity

--- apps/web/src/layouts/auth.tsx
+++ apps/web/src/layouts/auth.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 export function AuthPageLayout(): JSX.Element {
   return (
     <div className="flex items-center font-semibold">
-      <div className="flex flex-col items-start justify-between bg-primary w-[50%] h-screen p-10">
+      <div className="hidden sm:flex flex-col items-start justify-between bg-primary w-[50%] h-screen p-10">
         <div className="flex text-lg gap-3 items-center text-white">
           <Pyramid className="w-5 h-5" />
           <span className="text-2xl">Bank</span>
@@ -13,8 +13,8 @@ export function AuthPageLayout(): JSX.Element {
           Painel do parceiro © Bank - {new Date().getFullYear()}
         </footer>
       </div>
-      <div className="flex flex-col items-center justify-center w-[50vw] h-screen">
-        <section className="w-[35vw]">
+      <div className="flex flex-col items-center justify-center sm:w-[50vw] w-full h-screen">
+        <section className="sm:w-[35vw] w-full px-5">
           <Outlet />
         </section>
       </div>
