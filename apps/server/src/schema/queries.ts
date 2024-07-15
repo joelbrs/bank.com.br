3896e4755f910ed3a8b6b70e7ea1f48e382d162c
Joel Filho <joelfilho02812@gmail.com>
2024-07-15 11:50:16 -0300

    feat[server]: add transactions metrics graphql query

--- apps/server/src/schema/queries.ts
+++ apps/server/src/schema/queries.ts
@@ -1,12 +1,26 @@
 import { nodeField, nodesField } from "../modules/node";
-import { UserType } from "../modules/user";
-import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from "graphql";
+import { UserModel, UserType } from "../modules/user";
+import {
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from "graphql";
 import { connectionArgs, globalIdField } from "graphql-relay";
+import { isValidObjectId } from "mongoose";
+import { UnauthorizedException } from "../exceptions";
 import { AccountType } from "../modules/account/account-type";
-import { TransactionConnection, TransactionType } from "../modules/transaction";
+import { AccountModel } from "../modules/account";
+import {
+  TransactionConnection,
+  TransactionModel,
+  TransactionType,
+} from "../modules/transaction";
+import { MetricsType } from "@/modules/transaction/metrics-types";
 import { getUser } from "./queries/user";
 import { getAccount } from "./queries/account";
 import { getTransactionById, getUserTransactions } from "./queries/transaction";
+import { generateMonthlyMetrics, getMonthlyMetrics } from "./queries/metrics";
 
 export const Query = new GraphQLObjectType({
   name: "Query",
@@ -63,6 +77,19 @@ export const Query = new GraphQLObjectType({
       resolve: async (_, args, ctx) => {
         const transaction = await getTransactionById(ctx, args?._id as string);
         return transaction;
+      },
+    },
+    metrics: {
+      type: new GraphQLList(MetricsType),
+      resolve: async (_, __, ctx: any) => {
+        if (!ctx.user) {
+          throw new UnauthorizedException();
+        }
+
+        const userAccount = await getAccount(ctx, {});
+        const metrics = await getMonthlyMetrics(userAccount?._id as string);
+
+        return generateMonthlyMetrics(metrics);
       },
     },
   }),
