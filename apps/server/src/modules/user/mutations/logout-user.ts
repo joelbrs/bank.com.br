8442cc488a98567adbd046a957bff99d729da2fe
Joel Filho <joelfilho02812@gmail.com>
2024-07-15 08:18:05 -0300

    feat[server]: add logout graphql mutation

---
+++ apps/server/src/modules/user/mutations/logout-user.ts
@@ -0,0 +1,26 @@
+import { UnauthorizedException } from "@/exceptions";
+import { successField } from "@entria/graphql-mongo-helpers";
+import { GraphQLString } from "graphql";
+import { mutationWithClientMutationId } from "graphql-relay";
+
+export const LogoutUserMutation = mutationWithClientMutationId({
+  name: "Logout",
+  inputFields: {},
+  mutateAndGetPayload: (_, ctx) => {
+    if (!ctx.user) {
+      throw new UnauthorizedException();
+    }
+    ctx.ctx.cookies.set("bank.auth.token", undefined);
+
+    return {
+      message: "Operação realizada com sucesso!",
+    };
+  },
+  outputFields: {
+    message: {
+      type: GraphQLString,
+      resolve: async (payload) => (await payload)?.message,
+      ...successField,
+    },
+  },
+});
