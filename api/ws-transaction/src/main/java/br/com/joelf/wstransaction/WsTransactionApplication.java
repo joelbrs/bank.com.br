f4ace9930f860fbeaaee9375944c99d26f7cdd97
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2024-12-09 20:03:56 -0300

    chore(v2-api): init project

---
+++ api/ws-transaction/src/main/java/br/com/joelf/wstransaction/WsTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.wstransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsTransactionApplication.class, args);
+    }
+
+}
---
+++ api/ws-transaction/src/test/java/br/com/joelf/wstransaction/WsTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelf.wstransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsTransactionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
