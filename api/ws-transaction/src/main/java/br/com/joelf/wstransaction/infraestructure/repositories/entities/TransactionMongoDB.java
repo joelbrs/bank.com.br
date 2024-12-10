6f185ed8ebfc8f97c7940678a8147f8b208e1b2a
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2024-12-09 22:02:01 -0300

    feat(v2-api): add transaction mongo repository

---
+++ api/ws-transaction/src/main/java/br/com/joelf/wstransaction/infraestructure/repositories/entities/TransactionMongoDB.java
@@ -0,0 +1,33 @@
+package br.com.joelf.wstransaction.infraestructure.repositories.entities;
+
+import br.com.joelf.wstransaction.domain.entities.TransactionStatusEnum;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Document(collection = "transaction")
+public class TransactionMongoDB {
+
+    @Id
+    private Long id;
+    private TransactionStatusEnum status;
+    private String description;
+    private BigDecimal amount;
+    private String payerAccountIdentifier;
+    private String receiverAccountIdentifier;
+    private String idempotencyKey;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
