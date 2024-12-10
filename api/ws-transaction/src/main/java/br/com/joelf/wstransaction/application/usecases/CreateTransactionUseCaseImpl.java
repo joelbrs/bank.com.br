7a692b0f8a082294a682c64e6b9a36f0d9859718
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2024-12-09 22:05:21 -0300

    feat(v2-api): add create transaction use case impl

---
+++ api/ws-transaction/src/main/java/br/com/joelf/wstransaction/application/usecases/CreateTransactionUseCaseImpl.java
@@ -0,0 +1,39 @@
+package br.com.joelf.wstransaction.application.usecases;
+
+import br.com.joelf.wstransaction.application.dataprovider.PublisherDataProvider;
+import br.com.joelf.wstransaction.application.dataprovider.TransactionDataProvider;
+import br.com.joelf.wstransaction.domain.dtos.TransactionRequest;
+import br.com.joelf.wstransaction.domain.entities.Transaction;
+import br.com.joelf.wstransaction.domain.entities.TransactionStatusEnum;
+import br.com.joelf.wstransaction.domain.usecases.CreateTransactionUseCase;
+import br.com.joelf.wstransaction.domain.usecases.validations.Validation;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
+
+    private final Validation validation;
+
+    private final TransactionDataProvider transactionDataProvider;
+    private final PublisherDataProvider publisherDataProvider;
+
+    @Override
+    public void execute(TransactionRequest request) {
+        validation.validate(request);
+
+        //TODO: add exception treatments
+        Transaction result = transactionDataProvider.insert(buildTransaction(request));
+        publisherDataProvider.publishTransaction(result);
+    }
+
+    private Transaction buildTransaction(TransactionRequest request) {
+        return Transaction.builder()
+                .status(TransactionStatusEnum.PENDING)
+                .amount(request.getAmount())
+                .description(request.getDescription())
+                //TODO: add payerAccountIdentifier
+                .receiverAccountIdentifier(request.getAccountIdentifier())
+                .idempotencyKey(request.getIdempotencyKey())
+                .build();
+    }
+}
