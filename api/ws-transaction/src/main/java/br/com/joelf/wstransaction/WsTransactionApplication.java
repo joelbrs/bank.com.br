970360ef94fd02c2ad34a43e4efaba7f706e96a0
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2024-12-09 22:00:38 -0300

    chore(v2): ensure spring application is enabling rabbit and mongodb

--- api/ws-transaction/src/main/java/br/com/joelf/wstransaction/WsTransactionApplication.java
+++ api/ws-transaction/src/main/java/br/com/joelf/wstransaction/WsTransactionApplication.java
@@ -1,13 +1,16 @@
 package br.com.joelf.wstransaction;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableRabbit
+@EnableMongoRepositories
 @SpringBootApplication
 public class WsTransactionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WsTransactionApplication.class, args);
     }
-
 }
