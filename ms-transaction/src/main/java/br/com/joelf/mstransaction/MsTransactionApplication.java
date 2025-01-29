7f3553e3bbece015b4a55b1f568c64661a179cad
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-01-29 11:27:31 -0300

    chore: add spring to ms-transaction

---
+++ ms-transaction/src/main/java/br/com/joelf/mstransaction/MsTransactionApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.mstransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTransactionApplication.class, args);
+	}
+
+}
---
+++ ms-transaction/src/test/java/br/com/joelf/mstransaction/MsTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelf.mstransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
