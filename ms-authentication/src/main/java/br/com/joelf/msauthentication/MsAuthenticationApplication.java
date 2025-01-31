153dc855c026d334384bcdeed5e7ce20c8f9381e
Joel da Silva Pereira Filho <joelfilho02812@gmail.com>
2025-01-31 14:52:40 -0300

    chore(ms-auth): init project

---
+++ ms-authentication/src/main/java/br/com/joelf/msauthentication/MsAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelf.msauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAuthenticationApplication.class, args);
+    }
+
+}
---
+++ ms-authentication/src/test/java/br/com/joelf/msauthentication/MsAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joelf.msauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
